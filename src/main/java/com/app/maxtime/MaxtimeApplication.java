947b2c67b5887dd92cc4f46edace5f86f9318aea
RafaelTalavera <rtalavera@live.com.ar>
2024-05-23 11:08:22 -0300

    initial commit

---
+++ src/main/java/com/app/maxtime/MaxtimeApplication.java
@@ -0,0 +1,13 @@
+package com.app.maxtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/maxtime/MaxtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.maxtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
